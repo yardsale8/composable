fd5bc4a575f21e59982c14e0bf85bbc0891c07d5
Todd Iverson <tiverson@smumn.edu>
2020-10-13 08:42:48 -0500

    Added pipeable operators from toolz

--- composable/__init__.py
+++ composable/__init__.py
@@ -3,3 +3,8 @@ from composable.pipeable import (
 
     pipeable, 
     )
+
+from composable.utility import pipeable_all_in_module
+import toolz.curried.operator as operator
+
+pipeable_all_in_module(operator)
---
+++ tests/test_operator.py
@@ -0,0 +1,7 @@
+from composable import operator
+
+add = operator.add
+
+def test_add():
+    assert 2 >> add(3) == 5
+    assert "a" >> add("b") == "ba"
