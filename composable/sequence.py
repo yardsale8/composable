c1352e01abd518fc093fa1229b92a201f0297605
Todd Iverson <tiverson@smumn.edu>
2020-10-13 06:18:44 -0500

    Added head and tail, slice now works with iterable

--- composable/sequence.py
+++ composable/sequence.py
@@ -1,4 +1,5 @@
 from composable.pipeable import pipeable
+from itertools import islice
 
 @pipeable
 def to_list(iter):
@@ -36,19 +37,27 @@ built_in_slice = slice
 
 
 @pipeable
-def slice(from_, up_to, seq):
+def slice(start, stop, seq, step = 1):
     ''' Slices a seq, equivalent to seq[from_:up_to]
 
     Args:
-        from_: starting index for the slice.
-        up_to: final index
+        start: starting index for the slice.
+        stop: final index
+        step: the step size (can only be negative if seq supports slicing)
         seq: a sequence of value that supports slicing, e.g. list, tuple, or string
 
     Returns:
         A seq that is a subsequence of seq that starts at index `from_` 
         and includes all values up to, but not includeing `up_to`
+        The returned value will be the same type as the input if slicing is supported, 
+        otherwise this returns an iterable sequence.
     '''
-    return seq[built_in_slice(from_, up_to)]
+    try:
+        return seq[built_in_slice(start, stop, step)]
+    except:
+        if step <= 0:
+            raise ValueError("Step must be positive for iterators that don't support slicing.")
+        return islice(seq, start, stop, step)
 
 
 @pipeable
@@ -64,3 +73,39 @@ def join(sep, L):
         and includes all values up to, but not includeing `up_to`
     '''
     return sep.join(L)
+
+
+@pipeable
+def head(n, seq):
+    ''' Returns a list with the first n entries of seq, equivalent to seq[0:up_to]
+
+    Args:
+        from_: starting index for the slice.
+        up_to: final index
+        seq: a sequence of value that supports slicing, e.g. list, tuple, or string
+
+    Returns:
+        A seq that is a subsequence of seq that starts at index `from_` 
+        and includes all values up to, but not includeing `up_to`
+    '''
+    return slice(0, n, 1, seq)
+
+
+@pipeable
+def tail(n, seq):
+    ''' Returns a list with the last n entries of seq, equivalent to seq[0:up_to]
+
+    Args:
+        from_: starting index for the slice.
+        up_to: final index
+        seq: a sequence of value that supports slicing, e.g. list, tuple, or string
+
+    Returns:
+        A seq that is a subsequence of seq that starts at index `from_` 
+        and includes all values up to, but not includeing `up_to`
+    '''
+    try:
+        _ = len(seq)
+    except:
+        raise ValueError("tail needs a sequence that supports len and slicing")
+    return seq[built_in_slice(n, len(seq), 1)]
