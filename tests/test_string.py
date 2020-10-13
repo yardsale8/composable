f07ebc9f5dc5efa9432e33dfb59c4028c42c1cf0
Todd Iverson <tiverson@smumn.edu>
2020-10-13 06:20:37 -0500

    Tests for string functions

---
+++ tests/test_string.py
@@ -0,0 +1,18 @@
+import composable.string as s
+
+
+def test_split():
+    start = ', '.join(map(str, range(5)))
+    end = list(map(str, range(5)))
+    assert s.split(', ', start) == end
+    assert start >> s.split(', ') == end
+    assert s.split(', ', '') == ['']
+    assert '' >> s.split(', ') == ['']
+
+def test_split_re():
+    start = ', '.join(map(str, range(5)))
+    end = list(map(str, range(5)))
+    assert s.split_re(', ')(start) == end
+    assert start >> s.split_re(', ') == end
+    assert s.split_re(', ')('') == ['']
+    assert '' >> s.split_re(', ') == ['']
