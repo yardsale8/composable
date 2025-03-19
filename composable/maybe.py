51201dbcb83245cac4e40e1dd9e422157df016b9
Todd Iverson <Tiverson@winona.edu>
2025-03-19 09:17:35 -0500

    Updates to the maybe module

--- composable/maybe.py
+++ composable/maybe.py
@@ -1,49 +1,128 @@
 from composable import pipeable
-from typing import Optional, TypeVar, Callable
+# from composable.strict import map, filter
+# from composable.object import obj
+# from composable.utility import get
 
-A = TypeVar('A')
-B = TypeVar('B')
+class Nothing(object):
+    """A class representing nuothing
 
+    This class allows calling any attribute or calling like a function.
+    """
+    def __init__(self):
+        pass
+
+    def __getattr__(self, name):
+        return Nothing()
+
+    def __call__(self, *args, **kwargs):
+        return Nothing()
+
+    def __repr__(self):
+        return "Nothing()"
+
+
+class Just(object):
+    """A class representing a non-missing value
+
+    Any attribute or call will be passed to the underlying object.
+    """
+    def __init__(self, just):
+        self.just = just
+
+    def __getattr__(self, name):
+        return Just(getattr(self.just, name))
+
+    def __call__(self, *args, **kwargs):
+        return Just(self.just(*args, **kwargs))
+
+    def __repr__(self):
+        return f"Just({self.just.__repr__()})"
+
+# The maybe type, which is an alternative to Optional and 
+# Can be thought of as an applicative maybe monad.
+Maybe = Nothing | Just
 
 @pipeable
-def with_default(default: A, arg: Optional[A]) -> A:
-    ''' Provide a default value, turning an optional value into a normal value.
+def maybe(value):
+    """ Wraps a value  `Just` or returns `Nothing()` in place of `None`
 
     Args:
-        - default: Default to replace `None`
-        - opt: argument of type Optional[A]
+        value (Optional[T]): Any value that is either None or of type T
     
-    Returns: arg when arg is not None else default
-    >>> from composable.maybe import withDefault
-    >>> None >> with_default('a')
-    'a'
-    >>> 'b' >> with_default('a')
-    'b'
-    '''
-    return default if arg is None else arg
-
+    Returns (Maybe): Just(value) or Nothing()
+    """
+    return Nothing() if value is None else Just(value)
 
 @pipeable
-def map(f: Callable[[A], B], arg: Optional[A]) -> Optional[B]:
-    ''' Transform an optional value with a function.
+def unmaybe(value):
+    """ Extracts the optional value from an object of type Maybe
+
+    This function is the inverse of maybe.
 
     Args:
-        - f: function
-        - arg: optional value
+        value (Maybe): Returns value for Just(value) or None for Nothing()
 
-    Returns: f(arg) if arg is not None else None
+    Returns:
+        Optional(T): Returns the value wrapped by Just(value) or None 
+    """
+    return value.just if isinstance(value, Just) else None
 
-    Examples:
+_built_in_map = map
 
-    >>> from composable import maybe
-    >>> from toolz.curried.operator import add
-    >>> None >> maybe.map(add(2))
-    >>> None >> maybe.map(add(2)) is None
-    True
-    >>> 3 >> maybe.map(add(2))
-    5
-    >>> from composable.strict import map
-    >>> [1,2,None,4] >> map(maybe.map(add(2)))
-    [3, 4, None, 6]
-    ''' 
-    return f(arg) if arg is not None else None
+@pipeable
+def map(func, value):
+    """Maybes a functions to the value, when it exists
+
+    Args:
+        func (Callable[T, S]): A function that takes one argument of type T and returns an object of type S.
+        value (Maybe[T]): An optional value
+
+    Returns:
+        Maybe[T]: Returns the result of applying `func` to the optional value, or Nothing()
+    """
+    return Just(func(value.just)) if isinstance(value, Just) else Nothing()
+
+@pipeable
+def just_if(pred, value):
+    """Apply a filter to optional objects and keep just the values that pass.
+
+    Any value that (A) is Nothing() or (B) whose wrapped value fails the test return Nothing().
+
+    Args:
+        pred (Callable[T, bool]): A predicate functions for object of the wrapped type.
+        value (Maybe[T]): A wrapped optional value (Nothing() or Just(val))
+
+    Returns:
+        Maybe[T]: returns Just(val) if pred(val) otherwise Nothing() [pred(val) is false or value is Nothing()]
+    """
+    if isinstance(value, Just) and pred(value.just):
+        return value
+    else:
+        return Nothing()
+
+@pipeable
+def try_(func, value):
+    """Trys to apply a function to an optional wrapped value and returns Nothing() on any exception.
+
+    Args:
+        func (Callable[T, S]): A function applied to the optional wrapped value that may through an exception.
+        value (Maybe[T]): An optional wrapped value
+
+    Returns:
+        Maybe[T]: Returns Just(func(val)) when (A) the optional val exists and (B) func(val) doesn't throw an exception, returning Nothing() otherwise.
+    """
+    try:
+        return Just(func(value.just))
+    except:
+        return Nothing()
+
+@pipeable
+def is_just(value):
+    """Determines if an optional wrapped value exists."""
+    return isinstance(value, Just)
+
+@pipeable
+def is_nothing(value):
+    """Determines if an optional wrapped value is missing."""
+    return isinstance(value, Nothing)
+
